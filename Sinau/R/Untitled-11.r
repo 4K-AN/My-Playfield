631ad9dedfbaf54c72727de42c1bba9310bb386d
4K-AN <175827401+4K-AN@users.noreply.github.com>
2025-07-01 13:49:16 +0700

    Update Untitled-11.r

--- Sinau/R/Untitled-11.r
+++ Sinau/R/Untitled-11.r
@@ -6,8 +6,11 @@ kurang <- a - b
 kali   <- a * b
 bagi   <- a / b
 
+Mencoba <-- jumlah * (kurang * kali) + a^4 / (bagi * kurang) * a^2 + b^3 
+
+capture.output( Mencoba, file = "C:/Users/Asusg/Desktop/hasil.txt")
 print(jumlah)
 print(kurang)
 print(kali)
 print(bagi)
-
+print(Mencoba)

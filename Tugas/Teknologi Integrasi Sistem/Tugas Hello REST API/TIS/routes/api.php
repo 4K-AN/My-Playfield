59b3a7cddf2287556dbadb1d371a0bd84634485d
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2026-03-24 19:35:22 +0700

    Update api.php

--- Tugas/Teknologi Integrasi Sistem/Tugas Hello REST API/TIS/routes/api.php
+++ Tugas/Teknologi Integrasi Sistem/Tugas Hello REST API/TIS/routes/api.php
@@ -6,3 +6,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
+
+// a. Route Info
+Route::get('/info', function () {
+    return response()->json([
+        "app" => "Todo API",
+        "version" => "1.0",
+        "developer" => "Teknologi Informasi"
+    ]);
+});
+
+// b. Route User
+Route::get('/user/{name}', function ($name) {
+    return response()->json([
+        "message" => "Hello, " . $name . "!"
+    ]);
+});
+
+// c. Route Calculator
+Route::get('/calc/{a}/{b}/{op}', function ($a, $b, $op) {
+    $numA = (float) $a;
+    $numB = (float) $b;
+    $result = 0;
+
+    switch ($op) {
+        case 'add': $result = $numA + $numB; break;
+        case 'sub': $result = $numA - $numB; break;
+        case 'mul': $result = $numA * $numB; break;
+        case 'div': $result = ($numB != 0) ? $numA / $numB : "Error"; break;
+        default: return response()->json(["error" => "Invalid operation"], 400);
+    }
+
+    return response()->json([
+        "operation" => $op,
+        "a" => $numA,
+        "b" => $numB,
+        "result" => $result
+    ]);
+});

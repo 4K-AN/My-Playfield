e8da9197569d667e4704f7a9406eb60d5df0149c
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2026-04-07 20:36:57 +0700

    Praktikum_Mobile_4

---
+++ Tugas/Mobile/Praktikum4/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Menggunakan rememberSaveable agar kebal dari rotasi layar (Configuration Change)
+                    var number by rememberSaveable { mutableStateOf(1) }
+
+                    CounterScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(32.dp),
+                        number = number,
+                        label = "Double",
+                        onButtonClick = { number *= 2 }
+                    )
+                }
+            }
+        }
+    }
+}
+
+// Komponen Anak menjadi Stateless
+@Composable
+fun CounterScreen(modifier: Modifier, number: Int, label: String, onButtonClick: () -> Unit) {
+    Column(modifier) {
+        Text(text = "$number", fontSize = 72.sp)
+        Button(onClick = onButtonClick) {
+            Text(text = label)
+        }
+    }
+}

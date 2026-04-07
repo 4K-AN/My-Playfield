046d69cda5a4bf532a72722d15af8f0b340fcfdb
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2026-04-07 20:44:24 +0700

    4

--- Tugas/Mobile/Praktikum4/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Tugas/Mobile/Praktikum4/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -26,10 +29,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val snackbarHostState = remember { SnackbarHostState() }
+                var number by remember { mutableStateOf(1) }
 
-                    var number by rememberSaveable { mutableStateOf(1) }
+                // LaunchedEffect(Unit) hanya dijalankan sekali saat pertama kali masuk ke komposisi
+                LaunchedEffect(Unit) {
+                    number = Repo.getData()
+                }
 
+                // LaunchedEffect(number) dijalankan ulang setiap kali nilai 'number' berubah
+                LaunchedEffect(number) {
+                    snackbarHostState.showSnackbar("Number $number is shown!")
+                }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+                ) { innerPadding ->
                     CounterScreen(
                         modifier = Modifier
                             .padding(innerPadding)
@@ -44,7 +60,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// Komponen Anak menjadi Stateless
+// Komponen Anak tetap Stateless
 @Composable
 fun CounterScreen(modifier: Modifier, number: Int, label: String, onButtonClick: () -> Unit) {
     Column(modifier) {

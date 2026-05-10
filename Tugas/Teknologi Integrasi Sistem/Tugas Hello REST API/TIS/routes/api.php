24a8f1054ae2cb23cd0ed3272d03c99e55e9793b
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2026-05-10 21:19:58 +0700

    Update api.php

--- Tugas/Teknologi Integrasi Sistem/Tugas Hello REST API/TIS/routes/api.php
+++ Tugas/Teknologi Integrasi Sistem/Tugas Hello REST API/TIS/routes/api.php
@@ -15,7 +15,7 @@ Route::get('/info', function () {
         "developer" => "Teknologi Informasi"
     ]);
 });
-    
+
 // b. Route User
 // Route::get('/user/{name}', function ($name) {
 //     return response()->json([
@@ -30,11 +30,20 @@ Route::get('/calc/{a}/{b}/{op}', function ($a, $b, $op) {
     $result = 0;
 
     switch ($op) {
-        case 'add': $result = $numA + $numB; break;
-        case 'sub': $result = $numA - $numB; break;
-        case 'mul': $result = $numA * $numB; break;
-        case 'div': $result = ($numB != 0) ? $numA / $numB : "Error"; break;
-        default: return response()->json(["error" => "Invalid operation"], 400);
+        case 'add':
+            $result = $numA + $numB;
+            break;
+        case 'sub':
+            $result = $numA - $numB;
+            break;
+        case 'mul':
+            $result = $numA * $numB;
+            break;
+        case 'div':
+            $result = ($numB != 0) ? $numA / $numB : "Error";
+            break;
+        default:
+            return response()->json(["error" => "Invalid operation"], 400);
     }
 
     return response()->json([
@@ -59,26 +68,26 @@ Route::get('/students/{nim}/mata-kuliah', [App\Http\Controllers\StudentControlle
 Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 
-Route::middleware(['dummy.jwt'])->group(function() {
+Route::middleware(['dummy.jwt'])->group(function () {
     Route::get('/profile', [\App\Http\Controllers\Api\AuthController::class, 'profile']);
-    
-    Route::get('/admin/dashboard', function() {
+
+    Route::get('/admin/dashboard', function () {
         return response()->json([
             'message' => 'Welcome to Admin Dashboard'
         ]);
     })->middleware('role:admin');
-    
-    Route::get('/user/dashboard', function() {
+
+    Route::get('/user/dashboard', function () {
         return response()->json([
             'message' => 'Welcome to User Dashboard'
         ]);
     })->middleware('role:user');
-    
-    Route::get('/manager/dashboard', function() {
+
+    Route::get('/manager/dashboard', function () {
         return response()->json([
             'message' => 'Welcome to Manager Dashboard'
         ]);
     })->middleware('role:manager');
-    
+
     Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
 });

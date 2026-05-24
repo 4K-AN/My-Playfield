dd8ef1bc9f21c42f02859783d7f7360e3e87e2d2
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2026-05-24 21:26:29 +0700

    mobile

--- Tugas/Mobile/SupabaseRetrofit/app/src/main/java/com/example/supabaseretrofit/RetrofitInstance.kt
+++ Tugas/Mobile/SupabaseRetrofit/app/src/main/java/com/example/supabaseretrofit/RetrofitInstance.kt
@@ -4,12 +4,14 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitInstance {
-    private const val BASE_URL = "https://[project_id].supabase.co/rest/v1/"
+object
+
+RetrofitInstance {
+    private const val BASE_URL = "https://kepbltghhklkkldkbcjl.supabase.co/rest/v1/"
 
     private val retrofit: Retrofit by lazy {
         val sbHttpClient = OkHttpClient().newBuilder()
-            .addInterceptor(ApiKeyInterceptor("[publishable key]"))
+            .addInterceptor(ApiKeyInterceptor("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlcGJsdGdoaGtsa2tsZGtiY2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NjcxMTgsImV4cCI6MjA5NTE0MzExOH0.epd9elj1mxmUl2cn4Y_8GL4TfoXG6tXcNqwn5Id-r5Q"))
             .build()
 
         Retrofit.Builder()
